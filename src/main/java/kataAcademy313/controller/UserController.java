6bc155b574a2631dcf6960089f24fa11bdaa2a8b
Lev_Black <flah7796@gmail.com>
2022-11-03 15:27:02 +0500

    1.3

---
+++ src/main/java/kataAcademy313/controller/UserController.java
@@ -0,0 +1,31 @@
+package kataAcademy313.controller;
+
+import kataAcademy313.models.User;
+import kataAcademy313.services.RoleService;
+import kataAcademy313.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+    @GetMapping ()
+    public String viewUser(Model model, Principal principal){
+        User authUser = userService.getUserByUsername(principal.getName());
+        model.addAttribute("user", authUser);
+        return "user";
+    }
+}
